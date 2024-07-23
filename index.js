07e808cc603ccba97506ad96ba9ea86ff3b6feb2
Inkmu <sunxiangen@h-partners.com>
2024-07-23 09:59:07 +0800

    feat：Harmony next 适配

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* @flow */
 
-import { NativeModules } from 'react-native';
+import { NativeModules,TurboModuleRegistry } from 'react-native';
 
 const { RNVersionNumber } = NativeModules;
-
+const getVersion = TurboModuleRegistry.get("VersionNumberNativeModule");
 type VersionObject = {
   appVersion: ?string,
   buildVersion: ?string,
@@ -15,5 +15,11 @@ const VersionNumber: VersionObject = {
   buildVersion: RNVersionNumber && RNVersionNumber.buildVersion,
   bundleIdentifier: RNVersionNumber && RNVersionNumber.bundleIdentifier
 };
+
+if (Platform.OS == "harmony") {
+  VersionNumber.appVersion = getVersion.getAppVersion
+  VersionNumber.buildVersion = getVersion.getBuildVersion
+  VersionNumber.bundleIdentifier = getVersion.getBundleIdentifier
+}
 
 export default VersionNumber;
